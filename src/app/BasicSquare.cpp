b8ab77c6797d7e0310257dedc216a7b3e520eeec
Dr. Michael Reale <realemj@sunypoly.edu>
2026-08-25 21:27:47 -0400

    style: fix window title

--- src/app/BasicSquare.cpp
+++ src/app/BasicSquare.cpp
@@ -130,7 +130,7 @@ int main(int argc, char **argv) {
 
     // Set app name
     string appName = "BasicSquare";
-    string windowName = "Prometheus: " + appName;
+    string windowName = appName + ": realemj";
 
     ///////////////////////////////////////////////////////////////////////
     // GLFW
