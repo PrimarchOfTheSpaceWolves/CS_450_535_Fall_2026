188a454495b41a35e6fd654d6b8b38d18f032b1c
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-16 12:56:42 -0400

    feat: add hello world code

---
+++ src/app/Exercise03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#define PRO_USE_GLFW
+#include "pro/Prometheus.hpp"
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "BEGIN VULKAN EXERCISE" << endl;
+
+    // TODO
+
+    return 0;
+}
