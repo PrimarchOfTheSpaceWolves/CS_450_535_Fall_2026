c7681d9a801602e3f077bbc3dc69175d6b8dfe79
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-21 12:34:32 -0400

    feat: add basic drawing loop

--- src/app/Exercise03.cpp
+++ src/app/Exercise03.cpp
@@ -34,6 +34,9 @@ int main(int argc, char **argv) {
 
         pro::VulkanCore vkCore(coreCreateInfo);
 
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
         
         // TODO
     }
