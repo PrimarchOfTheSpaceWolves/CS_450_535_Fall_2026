1457cb5e5be96aee41d35de26186378bf1911e3f
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-21 13:43:44 -0400

    chore: add new exercise

---
+++ src/app/ProfExercise04.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#define PRO_USE_GLFW
+#include "pro/Prometheus.hpp"
+using namespace std;
+
+bool didWindowResize = false;
+
+static void window_resize_callback( GLFWwindow *window, 
+                                    int width, int height) {
+    didWindowResize = true;
+}
+
+int main(int argc, char **argv) {
+    cout << "BEGIN VULKAN EXERCISE" << endl;
+
+    if(!glfwInit()) {
+        cerr << "Error: GLFW did not init!" << endl;
+        exit(1);
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, true);
+
+    string appName = "ProfExercise04";
+    string windowTitle = appName + ": realemj";
+    GLFWwindow *window = glfwCreateWindow(800, 600, windowTitle.c_str(),
+                                            nullptr, nullptr);
+
+    if(!window) {
+        cerr << "Error: Could not create window!" << endl;
+        glfwTerminate();
+        exit(1);
+    }
+
+    glfwSetFramebufferSizeCallback(window, window_resize_callback);
+
+    {
+        pro::VulkanCoreCreateInfo coreCreateInfo {};
+        coreCreateInfo.appName = appName;
+
+        pro::prepareVulkanInitGLFWFunctions(coreCreateInfo, window);
+
+        pro::VulkanCore vkCore(coreCreateInfo);
+
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+
+            if(didWindowResize) {
+                cout << "Resized!" << endl;
+                vkCore.doWindowResize();
+                didWindowResize = false;
+            }
+        }
+        
+        // TODO
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
